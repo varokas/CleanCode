c069a3aef68839489a9ea7fb7cf873b83dfa3fe2
Varokas Panusuwan <varokas@panusuwan.com>
2014-06-04 16:22:25 -0700

    add a blank test

---
+++ src/test/java/com/thjug/meetup/CountUniqueIPTest.java
@@ -0,0 +1,10 @@
+package com.thjug.meetup;
+
+import org.junit.Test;
+
+public class CountUniqueIPTest {
+    @Test
+    public void placeHolder() throws Exception {
+        //TODO: Implment me
+    }
+}
